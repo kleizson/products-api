00980fa902baa9ef72bda309ccc2f6899ce94aea
kleizson <ksm1@ifal.edu.br>
2020-06-22 11:05:07 -0300

    fiz o create do controller de produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,52 @@ class ProductsController < ApplicationController
 
     end
 
+    def create
+
+        unless (params.has_key?(:name) && params.has_key?(:price) && params.has_key?(:description))
+            
+            return render json: {
+                error:{
+                    message: "Erro ao cadastrar produto! falta informações na requisição!"
+                }
+            },
+            status: :bad_request
+        
+        end
+
+        @product = Product.new(product_params)
+
+        
+        if  @product.save
+
+            render json: {
+                message: "Produto cadastrado com sucesso!",
+                produto: @product
+            },
+            status: :ok
+
+        else
+
+            render json: {
+                error:{
+                    message: "Erro ao cadastrar produto!"
+                }
+            },
+            status: :unprocessable_entity
+
+        end
+        
+        
+    end
+
+    
+    private
+
+    def product_params
+
+       params.permit(:name ,:price ,:description)
+
+    end
+
+
 end
