81b09665d8140472c328325aa3c2e419a5b77cb0
kleizson <ksm1@ifal.edu.br>
2020-06-22 10:51:16 -0300

    fiz o index do controller de produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+
+    def index
+        
+        @products = Product.order('created_at');
+
+        if @products.empty?
+            render json: {
+                message: "Lista de produtos vazia!!"
+            },
+            status: :ok
+        else
+            render json: {
+                produtos: @products
+            },
+            status: :ok
+        end
+
+    end
+
 end
